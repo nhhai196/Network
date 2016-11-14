aea05665f3c77c39e7c6ae296413534d4c469f8f
nhhai196 <nhhai196@gmail.com>
2016-11-14 12:46:45 -0500

    open

--- audiolisten.c
+++ audiolisten.c
@@ -208,7 +208,7 @@ int main(int argc, char * argv[]){
 	// TODO : sleep 2.5 seconds
 	
 	// Open file /dev/audio
-	fp = open("/dev/audio", O_RDWR, 0);
+	fp = open("/dev/audio0", O_RDWR, 0);
 	if (fp == -1){
 		perror("ERROR on open dev audio");
 		exit(1);
