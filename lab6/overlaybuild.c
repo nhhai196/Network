bd68c6a2840460ca27a428c267e1a34f19a83762
nhhai196 <nhhai196@gmail.com>
2016-12-03 22:19:07 -0500

    part1.1

--- lab6/overlaybuild.c
+++ lab6/overlaybuild.c
@@ -67,7 +67,8 @@ int main(int argc, char * argv[]){
 	their_addr.sin_port = htons(atoi(argv[argc-2]));
 	their_addr.sin_addr = *((struct in_addr *)he->h_addr);
 	bzero(&(their_addr.sin_zero), 8); // zero the rest of the struct
-
+	printf("Port : %ld", their_addr.sin_port);
+	
 	// Zero out buffer
 	memset(buffer, 0, BUFFSIZE);
 	
